94f9cb4eda0a6ae0ef65f67f7ce9b1d683192344
nambuitechx <nam.bui@techxcorp.com>
2025-03-17 10:21:14 +0000

    feat: update migration

--- backend/migrations/20250305022125_init_database.sql
+++ backend/migrations/20250305022125_init_database.sql
@@ -76,6 +76,37 @@ CREATE TABLE IF NOT EXISTS entity_relationship(
     json JSONB,
     deleted BOOLEAN NOT NULL
 );
+CREATE TABLE IF NOT EXISTS ingestion_pipeline_entity(
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(256) UNIQUE NOT NULL,
+    json JSONB NOT NULL,
+    updatedat BIGINT NOT NULL,
+    updatedby VARCHAR(256),
+    deleted BOOLEAN NOT NULL,
+    fqnhash VARCHAR(256),
+    timestamp BIGINT,
+    apptype VARCHAR(256),
+    pipelinetype VARCHAR(256)
+);
+CREATE TABLE IF NOT EXISTS pipeline_entity(
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(256) UNIQUE NOT NULL,
+    json JSONB NOT NULL,
+    updatedat BIGINT NOT NULL,
+    updatedby VARCHAR(256),
+    deleted BOOLEAN NOT NULL,
+    fqnhash VARCHAR(256)
+);
+CREATE TABLE IF NOT EXISTS pipeline_service_entity(
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(256) UNIQUE NOT NULL,
+    servicetype VARCHAR(256) NOT NULL,
+    json JSONB NOT NULL,
+    updatedat BIGINT NOT NULL,
+    updatedby VARCHAR(256),
+    deleted BOOLEAN NOT NULL,
+    namehash VARCHAR(256)
+);
 -- +goose StatementEnd
 
 -- +goose Down
@@ -89,4 +120,7 @@ DROP TABLE IF EXISTS automations_workflow;
 DROP TABLE IF EXISTS bot_entity;
 DROP TABLE IF EXISTS change_event;
 DROP TABLE IF EXISTS entity_relationship;
+DROP TABLE IF EXISTS ingestion_pipeline_entity;
+DROP TABLE IF EXISTS pipeline_entity;
+DROP TABLE IF EXISTS pipeline_service_entity;
 -- +goose StatementEnd
